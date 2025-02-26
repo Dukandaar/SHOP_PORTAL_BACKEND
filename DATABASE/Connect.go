f817722425af8f51dc3bcdac17a92057c35e9e6c
faithful dragon <faithfulldragon3@gmail.com>
2025-02-26 15:59:13 +0530

    .ignore file and added env

--- DATABASE/Connect.go
+++ DATABASE/Connect.go
@@ -27,7 +27,7 @@ func ConnectDB() *sql.DB {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	dbname := os.Getenv("DB_NAME")
-	utils.Logger.Info(logPrefix, user, password, host, port, dbname)
+
 	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", user, password, host, port, dbname)
 
 	DB, err = sql.Open(driverName, dsn)
